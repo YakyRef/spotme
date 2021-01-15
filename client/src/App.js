011f9908494e9fdbd702159d847d65b591013cf2
Yaky Refael <yaky.refael@axonius.com>
2021-01-15 21:16:12 +0200

    all together

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiResponse: "" };
+  // }
 
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }));
-  }
+  // callAPI() {
+  //   fetch("http://localhost:9000/testAPI")
+  //     .then((res) => res.text())
+  //     .then((res) => this.setState({ apiResponse: res }));
+  // }
 
-  componentWillMount() {
-    this.callAPI();
-  }
+  // componentWillMount() {
+  //   this.callAPI();
+  // }
   render() {
     return (
       <div className="App">
-        <p className="App-intro">;{this.state.apiResponse}</p>
+        <p className="App-intro">xxxxx</p>
+        <button onClick={this.hitBackend}>Send request</button>
       </div>
     );
   }
+  hitBackend(){
+    axios.get('/test')
+    .then((response) => {
+    console.log(response.data)
+  })
+ }
+    
 }
 
 export default App;
