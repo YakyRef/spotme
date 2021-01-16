9363232f28d7fea575deb1493f0199a35d3dcf07
Yaky Refael <yaky.refael@axonius.com>
2021-01-16 17:33:35 +0200

    fds

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicPath = path.join(__dirname, '../client', 'build');
+const publicPath = path.join(__dirname, '../client', 'public');
 const port = process.env.PORT || 3000;
 // const port = process.env.PORT || 8080;
 
